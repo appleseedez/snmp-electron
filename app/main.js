c7dd872c3bb827b2d5ce7be2fa9e0c959a907558
λ <Lyz@LyzdeMacBook-Pro.local>
2016-06-03 12:10:40 +0800

    20160604 electron start

---
+++ app/main.js
@@ -0,0 +1,33 @@
+const electron = require('electron')
+const { app } = electron
+const { BrowserWindow } = electron
+
+let win
+const createWindow = ()=>{
+  win = new BrowserWindow({
+    width:800,
+    height:600
+  })
+  win.loadURL(`file://${__dirname}/index.html`)
+  win.webContents.openDevTools({mode:'bottom'})
+
+
+
+  // events
+  win.on('closed',()=>{
+    win = null
+  })
+
+}
+
+app.on('ready',createWindow)
+app.on('window-all-closed',()=>{
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+app.on('activate',()=>{
+  if (win === null) {
+    createWindow()
+  }
+})
